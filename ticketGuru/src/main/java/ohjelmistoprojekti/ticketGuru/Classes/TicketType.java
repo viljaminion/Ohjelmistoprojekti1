2a274390b67dc1511f53250e1218053931e003dd
Heli Kyllinen <helikyllinen@gmail.com>
2024-02-11 21:15:03 +0200

    Lisätty ticketType luokka

---
+++ ticketGuru/src/main/java/ohjelmistoprojekti/ticketGuru/Classes/TicketType.java
@@ -0,0 +1,74 @@
+package ohjelmistoprojekti.ticketGuru.Classes;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class TicketType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private Long tickettypeid;
+
+    private String tickettype;
+    private String description;
+    private double price;
+
+    @OneToMany
+	(cascade = CascadeType.ALL, mappedBy = "ticket")
+	private List<Ticket>tickets;
+
+    public TicketType(){
+
+    }
+
+    public Long getId() {
+        return tickettypeid;
+    }
+
+    public void setId(Long tickettypeid) {
+		this.tickettypeid = tickettypeid;
+    }
+
+    public String getTickettype() {
+        return tickettype;
+    }
+
+    public void setTickettype(String tickettype) {
+		this.tickettype = tickettype;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+		this.description = description;
+    }
+
+    public double getPrice() {
+        return description;
+    }
+
+    public void setPrice(double price) {
+		this.price = price;
+    }
+
+    public List<Ticket> getTickets() {
+		return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+		this.tickets = tickets;
+    }
+
+    @Override
+		public String toString(){
+            return "TicketType [tickettypeid=" + tickettypeid + ", tickettype=" + tickettype + ", description=" + description + ",price=" + price + "]";
+        }
+
+}
