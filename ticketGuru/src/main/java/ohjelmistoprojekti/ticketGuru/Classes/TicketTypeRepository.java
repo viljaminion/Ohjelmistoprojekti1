bca74a3c4ba95a953cd2a2e6620c98cb4e1b5789
Heli Kyllinen <helikyllinen@gmail.com>
2024-02-12 16:05:41 +0200

    TicketType repo tehty

---
+++ ticketGuru/src/main/java/ohjelmistoprojekti/ticketGuru/Classes/TicketTypeRepository.java
@@ -0,0 +1,7 @@
+package ohjelmistoprojekti.ticketGuru.Classes;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TicketTypeRepository extends CrudRepository<TicketType, Long> {
+    TicketType findByTicketType(String tickettype);
+}
