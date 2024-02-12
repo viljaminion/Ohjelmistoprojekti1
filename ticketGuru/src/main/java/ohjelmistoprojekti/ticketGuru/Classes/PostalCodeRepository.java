66a0e8c524b22194ed0667e9ddefbfbc0fa47fda
viivisalin <salin.viivi@gmail.com>
2024-02-12 16:10:32 +0200

    PostalCode repo

---
+++ ticketGuru/src/main/java/ohjelmistoprojekti/ticketGuru/Classes/PostalCodeRepository.java
@@ -0,0 +1,7 @@
+package ohjelmistoprojekti.ticketGuru.Classes;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostalCodeRepository extends CrudRepository<PostalCode, Long> {
+    PostalCode findByPostalCode(String zipcode);
+}
