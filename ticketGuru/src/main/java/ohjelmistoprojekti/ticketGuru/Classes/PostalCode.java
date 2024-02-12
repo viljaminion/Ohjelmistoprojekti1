3e49135e18048be7f972877c910524debef85a5d
sakariarasola <sagary_97@hotmail.com>
2024-02-12 15:45:35 +0200

    PostalCode päivitetty

--- ticketGuru/src/main/java/ohjelmistoprojekti/ticketGuru/Classes/PostalCode.java
+++ ticketGuru/src/main/java/ohjelmistoprojekti/ticketGuru/Classes/PostalCode.java
@@ -1,9 +1,15 @@
 package ohjelmistoprojekti.ticketGuru.Classes;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 
 @Entity
@@ -11,16 +17,45 @@ public class PostalCode {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long zipcode;
+    private Long id;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private String zipcode;
 
     private String district;
 
-    public Long getZipId() {
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "postalcode")
+    private List<Customer> customers;
+
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "postalcode")
+    private List<Event> events;
+
+    //PITÄÄ TEHDÄ VIELÄ OneToMany LIPUN MYYJÄLLE
+
+    public PostalCode(String zipcode, String district) {
+        this.zipcode = zipcode;
+        this.district = district;
+    }
+
+    public PostalCode () {
+
+    }
+
+    public String getZipId() {
         return zipcode;
     }
 
-    public void setZipId(Long zipId) {
-        this.zipcode = zipId;
+    public void setZipId(String zipcode) {
+        this.zipcode = zipcode;
     }
 
     public String getDistrict() {
