75b98f4af803d0895c48f4b7ba0aac63ba9f3bb4
Viljami <viljamiolsen@hotmail.com>
2024-02-22 15:54:52 +0200

    SellerControlleriin lisätty importit

--- ticketGuru/src/main/java/ohjelmistoprojekti/ticketGuru/Controllers/SellerController.java
+++ ticketGuru/src/main/java/ohjelmistoprojekti/ticketGuru/Controllers/SellerController.java
@@ -1,10 +1,26 @@
 package ohjelmistoprojekti.ticketGuru.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ohjelmistoprojekti.ticketGuru.Classes.Seller;
+import ohjelmistoprojekti.ticketGuru.Classes.SellerRepository;
+import ohjelmistoprojekti.ticketGuru.Classes.TransactionRepository;
+
 @Controller
 public class SellerController {
 
     @Autowired
-    private SellerRepository sellerrepository;
+    private SellerRepository sellerRepository;
+    
+    @Autowired
+    private TransactionRepository transactionRepository;
 
     @RequestMapping("/sellerlist")
     public String sellerList(Model model) {
@@ -15,7 +31,7 @@ public class SellerController {
     @GetMapping("/sellers/add")
     public String addSeller(Model model) {
         model.addAttribute("seller", new Seller());
-        //model.addAttribute("transactions", transactionRepository.findAll());
+        model.addAttribute("transactions", transactionRepository.findAll());
         return "addSeller";
     }
 
@@ -27,13 +43,13 @@ public class SellerController {
 
     @GetMapping("/editSeller/{id}")
 	public String editSeller(@PathVariable("id") Long id, Model model) {
-		model.addAttribute("editSeller", SellerRepository.findById(id));
-		return "editBook"; 
+		model.addAttribute("editSeller", sellerRepository.findById(id));
+		return "editSeller"; 
 	}
 
     @GetMapping("/sellers/delete/{id}")
-    public String deleteSeller(@PathVariable Long sellerid) {
-        sellerRepository.deleteById(sellerid);
+    public String deleteSeller(@PathVariable Long id) {
+        sellerRepository.deleteById(id);
         return "redirect:/sellerlist";
     }
 
