ec4dff17753f7e2e13449556d88227fe402512e8
sakariarasola <sagary_97@hotmail.com>
2024-02-22 14:48:38 +0200

    Event korjattu

--- ticketGuru/src/main/java/ohjelmistoprojekti/ticketGuru/Classes/Event.java
+++ ticketGuru/src/main/java/ohjelmistoprojekti/ticketGuru/Classes/Event.java
@@ -1,13 +1,16 @@
 package ohjelmistoprojekti.ticketGuru.Classes;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Event {
