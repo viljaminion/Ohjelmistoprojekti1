cbbbfa4723946063a34a22359569b9e879eb6c08
Heli Kyllinen <helikyllinen@gmail.com>
2024-02-27 16:11:50 +0200

    SellerRestController tehty

---
+++ ticketGuru/src/main/java/ohjelmistoprojekti/ticketGuru/Controllers/SellerRestController.java
@@ -0,0 +1,61 @@
+package ohjelmistoprojekti.ticketGuru.Controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import ohjelmistoprojekti.ticketGuru.Classes.Seller;
+import ohjelmistoprojekti.ticketGuru.Classes.SellerRepository;
+
+
+@RestController
+public class SellerRestController {
+	
+	@Autowired
+    private SellerRepository sellerrepository;
+
+	
+//Kaikki tiedot JSON-muodossa
+	
+	
+    @RequestMapping(value="/sellers", method = RequestMethod.GET)
+    public List<Seller> SellerListRest() {	
+        return (List<Seller>) sellerrepository.findAll();
+    }    
+
+//ID:n avulla haettavat tiedot tietystä tapahtumasta
+    
+    @RequestMapping(value="/seller/{id}", method = RequestMethod.GET)
+    public Optional<Seller> findSellerRest(@PathVariable("id") Long sellerid) {	
+    	return sellerrepository.findById(sellerid);
+    }
+    
+//Tapahtuman lisääminen Postmanissa
+    
+    @RequestMapping (value= "/seller", method = RequestMethod.POST)
+    public Seller addSeller(@RequestBody Seller seller) {
+         return sellerrepository.save(seller);
+            
+}
+    
+//Tapahtuman poisto ID:llä Postmanissa
+    
+    @RequestMapping(value = "/seller/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<String> deleteSeller(@PathVariable("id") Long sellerid) {
+        Optional<Seller> seller = sellerrepository.findById(sellerid);
+        if (seller.isPresent()) {
+            sellerrepository.deleteById(sellerid);
+            return new ResponseEntity<>("Seller with ID " + seller + " has been deleted.", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Seller with ID " + seller + " not found.", HttpStatus.NOT_FOUND);
+        }
+    }
+}
