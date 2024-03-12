e2cf3602b2c5090ae159f9a315a349a36079b4af
sakariarasola <sagary_97@hotmail.com>
2024-03-12 16:51:44 +0200

    User ja WebSecurity lisätty, ei toimi vielä

---
+++ ticketGuru/src/main/java/ohjelmistoprojekti/ticketGuru/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package ohjelmistoprojekti.ticketGuru;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+// IMPORTTAA USERDETAILSERVICEIMPL
+
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+@Configuration
+public class WebSecurityConfig {
+
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(authorizeRequest -> authorizeRequest.requestMatchers(antMatcher("/")).permitAll()
+				//.requestMatchers(antMatcher("/ENDPOINTIT TÄNNE")).hasAuthority("ADMIN").anyRequest().authenticated())
+				.headers(headers -> headers.frameOptions(frameoptions -> frameoptions.disable()))
+				.formLogin(formlogin -> formlogin.defaultSuccessUrl("/recipelist", true).permitAll())
+				.logout(logout -> logout.permitAll());
+		return http.build();
+	}
+
+	@Autowired
+	//private UserDetailServiceImpl userDetailsService;
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		//auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+	}
+
+}
