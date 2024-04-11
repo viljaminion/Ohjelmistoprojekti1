2bbccf085d0fa5422ec8d1bc2021427d864eba78
leevi1231 <148748086+leevi1231@users.noreply.github.com>
2024-04-11 17:43:49 +0300

    julkasu ja etätietokanta konfigurointia part 8937582

--- ticketGuru/src/main/java/ohjelmistoprojekti/ticketGuru/domain/TicketRepository.java
+++ ticketGuru/src/main/java/ohjelmistoprojekti/ticketGuru/domain/TicketRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TicketRepository extends CrudRepository<Ticket, Long> {
-    @Query("SELECT t FROM Ticket t WHERE t.ticketid = ?1")
+    @Query("SELECT t FROM Ticket t WHERE t.ticket_id = ?1")
     Ticket findByTicketId(Long ticketId);
 }
