0b78170135463aea5b4b9108b9c6784c0edf6a18
leevi1231 <148748086+leevi1231@users.noreply.github.com>
2024-04-15 22:23:41 +0300

    koitan fixata julkasua kun ei ticketlist toimi siinä

--- ticketGuru/src/main/java/ohjelmistoprojekti/ticketGuru/web/AppUserController.java
+++ ticketGuru/src/main/java/ohjelmistoprojekti/ticketGuru/web/AppUserController.java
@@ -3,6 +3,7 @@ package ohjelmistoprojekti.ticketGuru.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import ohjelmistoprojekti.ticketGuru.domain.AppUser;
 import ohjelmistoprojekti.ticketGuru.domain.AppUserRepository;
 import ohjelmistoprojekti.ticketGuru.domain.TransactionRepository;
 
+@CrossOrigin
 @Controller
 public class AppUserController {
 
--- ticketGuru/src/main/java/ohjelmistoprojekti/ticketGuru/web/TicketRestController.java
+++ ticketGuru/src/main/java/ohjelmistoprojekti/ticketGuru/web/TicketRestController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +16,6 @@ import jakarta.validation.Valid;
 import ohjelmistoprojekti.ticketGuru.domain.Ticket;
 import ohjelmistoprojekti.ticketGuru.domain.TicketRepository;
 
-@CrossOrigin
 @RestController
 public class TicketRestController {
 
