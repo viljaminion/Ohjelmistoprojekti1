3d4c0149f2482835e3e9aac003db41501b93cfbc
leevi1231 <148748086+leevi1231@users.noreply.github.com>
2024-05-13 21:00:51 +0300

    gh pages säätö 234523

--- ticketGuruClient/vite.config.js
+++ ticketGuruClient/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Ohjelmistoprojekti1/ticketGuruClient/',
+  base: '/Ohjelmistoprojekti1/ticketguruClient/',
   plugins: [react()],
 })
